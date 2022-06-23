567cca8004ca64e9785b734819015e9ab29151dc
jonkur <jonkur@users.noreply.github.com>
2022-06-23 15:25:10 +0300

    v0.3, add endpoint for default frontpage for the app

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+//import path from 'path';
 
 const parsePortFromArgs = (args: Array<string>): number => {
   return args.length == 2 ? /^-[pP]$/.test(args[0]) && /^\d+$/.test(args[1]) ? parseInt(args[1]) : 0 : 0;
@@ -11,7 +12,7 @@ const args = process.argv.slice(2);
 const PORT = parsePortFromArgs(args) || process.env.PORT || 3001;
 
 app.get('/', (_req, res) => {
-  res.send('ping');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 app.listen(PORT, () => {
