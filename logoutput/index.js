fbf4e07ae0086d374973635217e1762cac120282
jonkur <jonkur@users.noreply.github.com>
2022-06-29 15:07:35 +0300

    Ex 1.07

--- logoutput/index.js
+++ logoutput/index.js
@@ -1,8 +1,38 @@
-//import { v4 as uuidv4 } from 'uuid';
+require('dotenv').config()
+const express = require('express')
 const uuid = require('uuid')
+
+const app = express()
+const log = []
+const logSize = 10
+const PORT = process.env.PORT || 3000
 
 str = uuid.v4();
 
-setInterval(() => {
-	console.log(new Date().toISOString() + ': ' + str);
-}, 5000);
+const storeLogLine = (logLine) => {
+	log.push(logLine)
+	if (log.length > logSize) {
+		log.shift()
+	}
+}
+
+const addLogLine = () => {
+	const tzoffset = (new Date()).getTimezoneOffset() * 60 * 1000
+	const line = (new Date(Date.now() - tzoffset)).toISOString() + ': ' + str
+	storeLogLine(line)
+	console.log(line)
+}
+
+app.get('/', (req, res) => {
+	res.send(log[log.length - 1])
+})
+
+app.listen(PORT, () => {
+	console.log(`App running in port ${PORT}`)
+	addLogLine()
+	setInterval(() => {
+		addLogLine()
+	}, 5000);
+})
+
+
